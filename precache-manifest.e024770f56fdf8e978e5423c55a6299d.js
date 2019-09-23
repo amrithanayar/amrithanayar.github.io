d2f6834d8ec87522286381084c430df3f31413e3
33059496 <amrithanayar@Amrithas-MBP.home>
2019-09-23 12:17:52 +0100

    Updates

---
+++ precache-manifest.e024770f56fdf8e978e5423c55a6299d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f977d055b758897f17d30f938a4ee3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8efe2245b1cef0cb57f1",
+    "url": "/static/css/main.13a52c51.chunk.css"
+  },
+  {
+    "revision": "5ea17a8412c18512ea3c",
+    "url": "/static/js/2.9e5810ae.chunk.js"
+  },
+  {
+    "revision": "8efe2245b1cef0cb57f1",
+    "url": "/static/js/main.06485149.chunk.js"
+  },
+  {
+    "revision": "e9f91ab79c8206bb9ebb",
+    "url": "/static/js/runtime-main.0507cff6.js"
+  }
+]);
