74d5e4c7ea617f23d498518585587b0139bb3296
33059496 <amrithanayar@Amrithas-MacBook-Pro.local>
2019-12-17 19:25:39 +0000

    Updates

---
+++ precache-manifest.3c6a5a6774be720339d4d154aea57de6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6bce0df5065076d5c376c346b1c6021",
+    "url": "/index.html"
+  },
+  {
+    "revision": "609cb8753eef292eb678",
+    "url": "/static/css/main.d27408a5.chunk.css"
+  },
+  {
+    "revision": "a3106e34bcb1a8346e6d",
+    "url": "/static/js/2.f6e658b6.chunk.js"
+  },
+  {
+    "revision": "609cb8753eef292eb678",
+    "url": "/static/js/main.d3f259f2.chunk.js"
+  },
+  {
+    "revision": "e9f91ab79c8206bb9ebb",
+    "url": "/static/js/runtime-main.0507cff6.js"
+  }
+]);
